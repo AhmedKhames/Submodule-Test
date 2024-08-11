8974e4f00bc25a14506e9bd07a24c29c18cc1666
ahmed.khames <ahmed.khames@widebot.net>
2024-08-11 13:12:37 +0300

    remove common 1

--- Common/Common/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Common
-{
-    public class Class1
-    {
-        public int MyProperty { get; set; }
-        public string sss { get; set; }
-        public int Age { get; set; }
-    }
-}
