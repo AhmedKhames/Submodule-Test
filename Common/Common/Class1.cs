c31d7377ae8b031e5f8881906a336956bacd9af3
ahmed.khames <ahmed.khames@widebot.net>
2024-08-11 12:58:00 +0300

    remove s

---
+++ Common/Common/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Common
+{
+    public class Class1
+    {
+        public int MyProperty { get; set; }
+        public string sss { get; set; }
+        public int Age { get; set; }
+    }
+}
