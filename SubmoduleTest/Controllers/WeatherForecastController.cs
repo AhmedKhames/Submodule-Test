a404694db62beef59a26077f90321910887acc45
ahmed.khames <ahmed.khames@widebot.net>
2024-08-11 13:00:09 +0300

    remove common folder

--- SubmoduleTest/Controllers/WeatherForecastController.cs
+++ SubmoduleTest/Controllers/WeatherForecastController.cs
@@ -1,4 +1,3 @@
-using Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SubmoduleTest.Controllers
@@ -22,8 +21,6 @@ namespace SubmoduleTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            Class1 class1 = new();
-            class1.sss = "dd";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
